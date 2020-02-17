d30431b99d3313f05c9d7edc68e0fb0f0de481e1
Pedro Peixoto <ppeixoto@usp.br>
2020-02-17 11:09:26 -0300

    documentation

--- model.py
+++ model.py
@@ -21,11 +21,16 @@ print(header)
 #----------------------------------
 D=params.D #diffusion coefficients
 K=params.K
+C=params.C
+xspace=params.x
 nspaces=len(D) #number of spaces
 nparts=len(K)
+
 print("You defined a mechanism with "+str(nspaces)+" space(s).")
+print("Mechanism layout (x): ",xspace)
+print("Initial concentrations:", C)
 print("Diffusion coefficients:", D)
-print("Partition coefficients:", K)
+print("Interface coefficients:", K)
 
 #Check dimensions
 if nparts!=nspaces-1 and nspaces>1:
@@ -33,5 +38,40 @@ if nparts!=nspaces-1 and nspaces>1:
     print("Please re-configure parameters in params.py")
     sys.exit(-1)
 
+#Discretize space
+n = 10
+dxspace = np.array([j-i for i, j in zip(xspace[:-1], xspace[1:])])
+mindxspace=min(dxspace)
+dx=mindxspace/n
+a = xspace[0]
+b = xspace[-1]
+N = int((b-a)/dx)
+x = np.linspace(a,b, N+1)
+ind_space = (N*dxspace/(b-a)).astype(int)
 
+#Discretize time
+T = params.maxtime
+maxD = max(D)
+dt = 0.25*dx*dx/maxD
+Nt = int(T/dt)
+t = np.linspace(0, T, Nt+1)
 
+F = dt/(dx*dx)
+
+unew   = np.zeros(N+1) # unknown u at t+1
+uold = np.zeros(N+1)   # u at t
+
+# Set initial condition u(x,0) = I(x)
+for i in range(0, N+1):
+    u_1[i] = I(x[i])
+
+for n in range(0, Nt):
+    # Compute u at inner mesh points
+    for i in range(1, Nx):
+        u[i] = u_1[i] + F*(u_1[i-1] - 2*u_1[i] + u_1[i+1])
+
+    # Insert boundary conditions
+    u[0] = 0;  u[Nx] = 0
+
+    # Update u_1 before next step
+    u_1[:]= u
--- params.py
+++ params.py
@@ -9,21 +9,23 @@ import numpy as np
 # - mass: micro-grams (mug)
 # - volume: cm^3 (mL)
 
+#Domain definition (position of interfaces) 
+# x=np.array([initial_position, interface_1, interface_2, final_position])
+#x=np.array([0, 1]) # cm
+x=np.array([0, 50, 100]) # cm
+
+#Initial concentrations in each space
+#  Assumed constant for each space
+C=np.array([1.0, 0.0]) #mug/mL
+
 #Diffusion coefficients for each space - manually defined
-D=np.array([1]) # 1 space (cm^2/s)
-#D=np.array([1, 0.5]) # 2 spaces
-#D=np.array([1, 0.5, 0.3]) # 3 spaces
+D=np.array([1.0, 2.0]) # 2 spaces (cm^2/s)
 
 #Partition coefficients K for each interface - manually defined 
 K=np.array([1]) # 1 interface, non-dimensional
 
-#Initial concentrations in each space
-#  Assumed constant for each space
-C=np.array([1]) #mug/mL
 
-#Domain definition (position of interfaces) 
-# x=np.array([initial_position, interface_1, interface_2, final_position])
-x=np.array([0, 1]) # cm
+
 
 #Max time definition
 maxtime = 10*60 #10 minutes
