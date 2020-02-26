ef2cb8666e11ca0adfad3b79f5ad2bb715e1ffb1
Pedro Peixoto <ppeixoto@usp.br>
2020-02-25 21:28:13 -0300

    zhang1993

--- mex_params.py
+++ mex_params.py
@@ -13,33 +13,42 @@ import numpy as np
 #x=np.array([0, 1]) # cm
 # x0   x1   x2
 # |----|----|
-x=np.array([0, 0.5, 1]) # cm
+#x=np.array([0, 0.5, 1]) # cm
+x=np.array([0, 0.0056, 0.07, 0.1]) # cm Zhang 1993 Fig 2
+
+#Names of compartments
+#xnames = ["", "", ""]
+xnames = ["coating", "headspace", "aqueous"]
 
 #Initial concentrations in each space
 #  Assumed constant for each space
-#   C0   C1
-# |----|----|
-C=np.array([0.0, 1.0]) #mug/mL
+#   C0   C1   C2
+# |----|----|----|
+#C=np.array([0.0, 1.0]) #mug/mL
+C=np.array([0.0, 0.0, 1.0]) #mug/mL Zhang 1993 Fig 2
 
 #Diffusion coefficients for each space - manually defined
-#   D0   D1
-# |----|----|
-D=np.array([2.0, 1.0]) # 2 spaces (cm^2/s)
+#   D0   D1   D2
+# |----|----|----|
+#D=np.array([2.0, 1.0]) # 2 spaces (cm^2/s)
+D=np.array([2.8e-6, 0.077, 1.8e-5]) # (cm^2/s) Zhang 1993 Fig 2
 
 #Partition coefficients K for each interface - manually defined 
-# Set 1.0 for beggining and endpoints    
-# K0  K1  K2
-# |---|---|
-K=np.array([0.0, 2.0, 0.0]) # 2 boundaries and 1 interface coefficient, non-dimensional
+# Set 0.0 for beggining and endpoints    
+# K0  K1  K2   K3
+# |---|---|----|
+#K=np.array([0.0, 2.0, 0.0]) # 2 boundaries and 1 interface coefficient, non-dimensional
+K=np.array([0.0, 50.0, 0.2, 0.0]) # Zhang 1993 Fig 2
 
 #Max time definition
-maxtime = 100*60 #10 minutes
+maxtime = 10*60 #10 minutes
+maxtime = 60 #seconds
 
-#Plotting time interval (every XX time units)
-iplot_time=10
+#Plotting time spots
+iplot_time=np.array([0.0, 3.0, 15.0, 30.0, 60.0])
 
 #Space discretization (number of grid points)
-N = 10
+N = 1000
 
 #Experiment basename
-name = "microextraction"
+name = "microextraction_zhang1993_fig2"
--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import scipy as sp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import sys
-mpl.use('Agg')
+plt.rcParams.update({'font.size': 18})
 
 #Main model file
 import mextractmodel as mex
@@ -22,28 +22,43 @@ p=mex.device()
 T=params.maxtime
 maxD = max(p.D)
 dx = p.domain_len/p.N
-dt = 0.1*dx/maxD #0.25*dx*dx/maxD
+dt = 0.1 #0.1*dx/maxD #0.25*dx*dx/maxD
 Nt = int(T/dt)
 time = np.linspace(0, T, Nt+1)
+print("\nTime-space info (dx, dt, Nt, maxD, dx/maxD):\n", dx, dt, Nt, maxD, dx/maxD, "\n")
 iplot=params.iplot_time
 
+#Figure parameters
 fig, axes = plt.subplots(1,1, constrained_layout=True, figsize=(15,10))
-plt.xlabel("Device position (cm)")
-plt.ylabel("Concentration")
+plt.xlabel("x-distance (cm)")
+plt.ylabel("Concentration ($\mu g/mL$)")
+plt.title("Microextration Model")
+for i, name in enumerate(params.xnames):
+    x=0.8*params.x[i]+0.2*params.x[i+1]
+    plt.text(x, -0.1, name)
 
 #plt.pause(0.05)
 #plt.show()
-print("i    time     mass")
+
 
 #loop over time
+print("i    time     mass")
 for i, t in enumerate(time):    
-    if i%iplot == 0:
-        print(i,t, p.mass) #, p.u, p.uext)
-        axes.plot(p.x, p.uext)
-        plt.title("Microextration t="+str(t) )
-        #plt.pause(0.05)
-        istr="{:07.0f}".format(i)
+
+    #Plot if required
+    #if i%iplot == 0:
+    if t in iplot:
+        print(i,t, p.mass)
+
+        #Plot
+        axes.plot(p.x, p.uext, label=str(t)+"s")
+        axes.legend()
+        istr="{:07.0f}".format(t)
         plt.savefig(p.basename+"_"+istr+".png")
+        #plt.pause(0.05)
+
+    #Run time step
     p.run_timestep(dt)
+
+
 #plt.show()
-plt.savefig(p.basename+"_"+str(t)+".png")
